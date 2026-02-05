a8b57cf3e4a7b7f3515e8173ac7bf2e11d6defb1
Tejas C <tejas.c2023@vitstudent.ac.in>
2026-02-05 12:43:48 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,14 +1,5 @@
-import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the name: ");
-        String name = sc.nextLine();
-        System.out.print("Enter the age: ");
-        int age = sc.nextInt();
-        System.out.println("\n***Student Details***");
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        sc.close();
+        System.out.println("MINI CI Project")
     }
 }
